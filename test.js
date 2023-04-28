91e5a91f614a552896f8d6ef18a3a820111c5e84
Mlitliu0213 <130287136+Mlitliu0213@users.noreply.github.com>
2023-04-28 17:03:14 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -2,199 +2,416 @@
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
 (function() {
-  var mode = CodeMirror.getMode({indentUnit: 2}, "javascript");
+  var mode = CodeMirror.getMode({tabSize: 4}, 'textile');
   function MT(name) { test.mode(name, mode, Array.prototype.slice.call(arguments, 1)); }
 
-  MT("locals",
-     "[keyword function] [variable foo]([def a], [def b]) { [keyword var] [def c] [operator =] [number 10]; [keyword return] [variable-2 a] [operator +] [variable-2 c] [operator +] [variable d]; }");
+  MT('simpleParagraphs',
+      'Some text.',
+      '',
+      'Some more text.');
 
-  MT("comma-and-binop",
-     "[keyword function](){ [keyword var] [def x] [operator =] [number 1] [operator +] [number 2], [def y]; }");
+  /*
+   * Phrase Modifiers
+   */
 
-  MT("destructuring",
-     "([keyword function]([def a], [[[def b], [def c] ]]) {",
-     "  [keyword let] {[def d], [property foo]: [def c][operator =][number 10], [def x]} [operator =] [variable foo]([variable-2 a]);",
-     "  [[[variable-2 c], [variable y] ]] [operator =] [variable-2 c];",
-     "})();");
+  MT('em',
+      'foo [em _bar_]');
 
-  MT("class_body",
-     "[keyword class] [variable Foo] {",
-     "  [property constructor]() {}",
-     "  [property sayName]() {",
-     "    [keyword return] [string-2 `foo${][variable foo][string-2 }oo`];",
-     "  }",
-     "}");
+  MT('emBoogus',
+      'code_mirror');
 
-  MT("class",
-     "[keyword class] [variable Point] [keyword extends] [variable SuperThing] {",
-     "  [property get] [property prop]() { [keyword return] [number 24]; }",
-     "  [property constructor]([def x], [def y]) {",
-     "    [keyword super]([string 'something']);",
-     "    [keyword this].[property x] [operator =] [variable-2 x];",
-     "  }",
-     "}");
+  MT('strong',
+      'foo [strong *bar*]');
 
-  MT("module",
-     "[keyword module] [string 'foo'] {",
-     "  [keyword export] [keyword let] [def x] [operator =] [number 42];",
-     "  [keyword export] [keyword *] [keyword from] [string 'somewhere'];",
-     "}");
+  MT('strongBogus',
+      '3 * 3 = 9');
 
-  MT("import",
-     "[keyword function] [variable foo]() {",
-     "  [keyword import] [def $] [keyword from] [string 'jquery'];",
-     "  [keyword module] [def crypto] [keyword from] [string 'crypto'];",
-     "  [keyword import] { [def encrypt], [def decrypt] } [keyword from] [string 'crypto'];",
-     "}");
+  MT('italic',
+      'foo [em __bar__]');
 
-  MT("const",
-     "[keyword function] [variable f]() {",
-     "  [keyword const] [[ [def a], [def b] ]] [operator =] [[ [number 1], [number 2] ]];",
-     "}");
+  MT('italicBogus',
+      'code__mirror');
 
-  MT("for/of",
-     "[keyword for]([keyword let] [variable of] [keyword of] [variable something]) {}");
+  MT('bold',
+      'foo [strong **bar**]');
 
-  MT("generator",
-     "[keyword function*] [variable repeat]([def n]) {",
-     "  [keyword for]([keyword var] [def i] [operator =] [number 0]; [variable-2 i] [operator <] [variable-2 n]; [operator ++][variable-2 i])",
-     "    [keyword yield] [variable-2 i];",
-     "}");
+  MT('boldBogus',
+      '3 ** 3 = 27');
 
-  MT("quotedStringAddition",
-     "[keyword let] [variable f] [operator =] [variable a] [operator +] [string 'fatarrow'] [operator +] [variable c];");
+  MT('simpleLink',
+      '[link "CodeMirror":http://codemirror.net]');
 
-  MT("quotedFatArrow",
-     "[keyword let] [variable f] [operator =] [variable a] [operator +] [string '=>'] [operator +] [variable c];");
+  MT('referenceLink',
+      '[link "CodeMirror":code_mirror]',
+      'Normal Text.',
+      '[link [[code_mirror]]http://codemirror.net]');
 
-  MT("fatArrow",
-     "[variable array].[property filter]([def a] [operator =>] [variable-2 a] [operator +] [number 1]);",
-     "[variable a];", // No longer in scope
-     "[keyword let] [variable f] [operator =] ([[ [def a], [def b] ]], [def c]) [operator =>] [variable-2 a] [operator +] [variable-2 c];",
-     "[variable c];");
+  MT('footCite',
+      'foo bar[qualifier [[1]]]');
 
-  MT("spread",
-     "[keyword function] [variable f]([def a], [meta ...][def b]) {",
-     "  [variable something]([variable-2 a], [meta ...][variable-2 b]);",
-     "}");
+  MT('footCiteBogus',
+      'foo bar[[1a2]]');
 
-  MT("comprehension",
-     "[keyword function] [variable f]() {",
-     "  [[([variable x] [operator +] [number 1]) [keyword for] ([keyword var] [def x] [keyword in] [variable y]) [keyword if] [variable pred]([variable-2 x]) ]];",
-     "  ([variable u] [keyword for] ([keyword var] [def u] [keyword of] [variable generateValues]()) [keyword if] ([variable-2 u].[property color] [operator ===] [string 'blue']));",
-     "}");
+  MT('special-characters',
+          'Registered [tag (r)], ' +
+          'Trademark [tag (tm)], and ' +
+          'Copyright [tag (c)] 2008');
 
-  MT("quasi",
-     "[variable re][string-2 `fofdlakj${][variable x] [operator +] ([variable re][string-2 `foo`]) [operator +] [number 1][string-2 }fdsa`] [operator +] [number 2]");
+  MT('cite',
+      "A book is [keyword ??The Count of Monte Cristo??] by Dumas.");
 
-  MT("quasi_no_function",
-     "[variable x] [operator =] [string-2 `fofdlakj${][variable x] [operator +] [string-2 `foo`] [operator +] [number 1][string-2 }fdsa`] [operator +] [number 2]");
+  MT('additionAndDeletion',
+      'The news networks declared [negative -Al Gore-] ' +
+        '[positive +George W. Bush+] the winner in Florida.');
 
-  MT("indent_statement",
-     "[keyword var] [variable x] [operator =] [number 10]",
-     "[variable x] [operator +=] [variable y] [operator +]",
-     "  [atom Infinity]",
-     "[keyword debugger];");
+  MT('subAndSup',
+      'f(x, n) = log [builtin ~4~] x [builtin ^n^]');
 
-  MT("indent_if",
-     "[keyword if] ([number 1])",
-     "  [keyword break];",
-     "[keyword else] [keyword if] ([number 2])",
-     "  [keyword continue];",
-     "[keyword else]",
-     "  [number 10];",
-     "[keyword if] ([number 1]) {",
-     "  [keyword break];",
-     "} [keyword else] [keyword if] ([number 2]) {",
-     "  [keyword continue];",
-     "} [keyword else] {",
-     "  [number 10];",
-     "}");
+  MT('spanAndCode',
+      'A [quote %span element%] and [atom @code element@]');
 
-  MT("indent_for",
-     "[keyword for] ([keyword var] [variable i] [operator =] [number 0];",
-     "     [variable i] [operator <] [number 100];",
-     "     [variable i][operator ++])",
-     "  [variable doSomething]([variable i]);",
-     "[keyword debugger];");
+  MT('spanBogus',
+      'Percentage 25% is not a span.');
 
-  MT("indent_c_style",
-     "[keyword function] [variable foo]()",
-     "{",
-     "  [keyword debugger];",
-     "}");
+  MT('citeBogus',
+      'Question? is not a citation.');
 
-  MT("indent_else",
-     "[keyword for] (;;)",
-     "  [keyword if] ([variable foo])",
-     "    [keyword if] ([variable bar])",
-     "      [number 1];",
-     "    [keyword else]",
-     "      [number 2];",
-     "  [keyword else]",
-     "    [number 3];");
+  MT('codeBogus',
+      'user@example.com');
 
-  MT("indent_funarg",
-     "[variable foo]([number 10000],",
-     "    [keyword function]([def a]) {",
-     "  [keyword debugger];",
-     "};");
+  MT('subBogus',
+      '~username');
 
-  MT("indent_below_if",
-     "[keyword for] (;;)",
-     "  [keyword if] ([variable foo])",
-     "    [number 1];",
-     "[number 2];");
+  MT('supBogus',
+      'foo ^ bar');
 
-  MT("multilinestring",
-     "[keyword var] [variable x] [operator =] [string 'foo\\]",
-     "[string bar'];");
+  MT('deletionBogus',
+      '3 - 3 = 0');
 
-  MT("scary_regexp",
-     "[string-2 /foo[[/]]bar/];");
+  MT('additionBogus',
+      '3 + 3 = 6');
 
-  MT("indent_strange_array",
-     "[keyword var] [variable x] [operator =] [[",
-     "  [number 1],,",
-     "  [number 2],",
-     "]];",
-     "[number 10];");
+  MT('image',
+      'An image: [string !http://www.example.com/image.png!]');
 
-  var jsonld_mode = CodeMirror.getMode(
-    {indentUnit: 2},
-    {name: "javascript", jsonld: true}
-  );
-  function LD(name) {
-    test.mode(name, jsonld_mode, Array.prototype.slice.call(arguments, 1));
-  }
+  MT('imageWithAltText',
+      'An image: [string !http://www.example.com/image.png (Alt Text)!]');
 
-  LD("json_ld_keywords",
-    '{',
-    '  [meta "@context"]: {',
-    '    [meta "@base"]: [string "http://example.com"],',
-    '    [meta "@vocab"]: [string "http://xmlns.com/foaf/0.1/"],',
-    '    [property "likesFlavor"]: {',
-    '      [meta "@container"]: [meta "@list"]',
-    '      [meta "@reverse"]: [string "@beFavoriteOf"]',
-    '    },',
-    '    [property "nick"]: { [meta "@container"]: [meta "@set"] },',
-    '    [property "nick"]: { [meta "@container"]: [meta "@index"] }',
-    '  },',
-    '  [meta "@graph"]: [[ {',
-    '    [meta "@id"]: [string "http://dbpedia.org/resource/John_Lennon"],',
-    '    [property "name"]: [string "John Lennon"],',
-    '    [property "modified"]: {',
-    '      [meta "@value"]: [string "2010-05-29T14:17:39+02:00"],',
-    '      [meta "@type"]: [string "http://www.w3.org/2001/XMLSchema#dateTime"]',
-    '    }',
-    '  } ]]',
-    '}');
+  MT('imageWithUrl',
+      'An image: [string !http://www.example.com/image.png!:http://www.example.com/]');
 
-  LD("json_ld_fake",
-    '{',
-    '  [property "@fake"]: [string "@fake"],',
-    '  [property "@contextual"]: [string "@identifier"],',
-    '  [property "user@domain.com"]: [string "@graphical"],',
-    '  [property "@ID"]: [string "@@ID"]',
-    '}');
+  /*
+   * Headers
+   */
+
+  MT('h1',
+      '[header&header-1 h1. foo]');
+
+  MT('h2',
+      '[header&header-2 h2. foo]');
+
+  MT('h3',
+      '[header&header-3 h3. foo]');
+
+  MT('h4',
+      '[header&header-4 h4. foo]');
+
+  MT('h5',
+      '[header&header-5 h5. foo]');
+
+  MT('h6',
+      '[header&header-6 h6. foo]');
+
+  MT('h7Bogus',
+      'h7. foo');
+
+  MT('multipleHeaders',
+      '[header&header-1 h1. Heading 1]',
+      '',
+      'Some text.',
+      '',
+      '[header&header-2 h2. Heading 2]',
+      '',
+      'More text.');
+
+  MT('h1inline',
+      '[header&header-1 h1. foo ][header&header-1&em _bar_][header&header-1  baz]');
+
+  /*
+   * Lists
+   */
+
+  MT('ul',
+      'foo',
+      'bar',
+      '',
+      '[variable-2 * foo]',
+      '[variable-2 * bar]');
+
+  MT('ulNoBlank',
+      'foo',
+      'bar',
+      '[variable-2 * foo]',
+      '[variable-2 * bar]');
+
+  MT('ol',
+      'foo',
+      'bar',
+      '',
+      '[variable-2 # foo]',
+      '[variable-2 # bar]');
+
+  MT('olNoBlank',
+      'foo',
+      'bar',
+      '[variable-2 # foo]',
+      '[variable-2 # bar]');
+
+  MT('ulFormatting',
+      '[variable-2 * ][variable-2&em _foo_][variable-2  bar]',
+      '[variable-2 * ][variable-2&strong *][variable-2&em&strong _foo_]' +
+        '[variable-2&strong *][variable-2  bar]',
+      '[variable-2 * ][variable-2&strong *foo*][variable-2  bar]');
+
+  MT('olFormatting',
+      '[variable-2 # ][variable-2&em _foo_][variable-2  bar]',
+      '[variable-2 # ][variable-2&strong *][variable-2&em&strong _foo_]' +
+        '[variable-2&strong *][variable-2  bar]',
+      '[variable-2 # ][variable-2&strong *foo*][variable-2  bar]');
+
+  MT('ulNested',
+      '[variable-2 * foo]',
+      '[variable-3 ** bar]',
+      '[keyword *** bar]',
+      '[variable-2 **** bar]',
+      '[variable-3 ** bar]');
+
+  MT('olNested',
+      '[variable-2 # foo]',
+      '[variable-3 ## bar]',
+      '[keyword ### bar]',
+      '[variable-2 #### bar]',
+      '[variable-3 ## bar]');
+
+  MT('ulNestedWithOl',
+      '[variable-2 * foo]',
+      '[variable-3 ## bar]',
+      '[keyword *** bar]',
+      '[variable-2 #### bar]',
+      '[variable-3 ** bar]');
+
+  MT('olNestedWithUl',
+      '[variable-2 # foo]',
+      '[variable-3 ** bar]',
+      '[keyword ### bar]',
+      '[variable-2 **** bar]',
+      '[variable-3 ## bar]');
+
+  MT('definitionList',
+      '[number - coffee := Hot ][number&em _and_][number  black]',
+      '',
+      'Normal text.');
+
+  MT('definitionListSpan',
+      '[number - coffee :=]',
+      '',
+      '[number Hot ][number&em _and_][number  black =:]',
+      '',
+      'Normal text.');
+
+  MT('boo',
+      '[number - dog := woof woof]',
+      '[number - cat := meow meow]',
+      '[number - whale :=]',
+      '[number Whale noises.]',
+      '',
+      '[number Also, ][number&em _splashing_][number . =:]');
+
+  /*
+   * Attributes
+   */
+
+  MT('divWithAttribute',
+      '[punctuation div][punctuation&attribute (#my-id)][punctuation . foo bar]');
+
+  MT('divWithAttributeAnd2emRightPadding',
+      '[punctuation div][punctuation&attribute (#my-id)((][punctuation . foo bar]');
+
+  MT('divWithClassAndId',
+      '[punctuation div][punctuation&attribute (my-class#my-id)][punctuation . foo bar]');
+
+  MT('paragraphWithCss',
+      'p[attribute {color:red;}]. foo bar');
+
+  MT('paragraphNestedStyles',
+      'p. [strong *foo ][strong&em _bar_][strong *]');
+
+  MT('paragraphWithLanguage',
+      'p[attribute [[fr]]]. Parlez-vous français?');
+
+  MT('paragraphLeftAlign',
+      'p[attribute <]. Left');
+
+  MT('paragraphRightAlign',
+      'p[attribute >]. Right');
+
+  MT('paragraphRightAlign',
+      'p[attribute =]. Center');
+
+  MT('paragraphJustified',
+      'p[attribute <>]. Justified');
+
+  MT('paragraphWithLeftIndent1em',
+      'p[attribute (]. Left');
+
+  MT('paragraphWithRightIndent1em',
+      'p[attribute )]. Right');
+
+  MT('paragraphWithLeftIndent2em',
+      'p[attribute ((]. Left');
+
+  MT('paragraphWithRightIndent2em',
+      'p[attribute ))]. Right');
+
+  MT('paragraphWithLeftIndent3emRightIndent2em',
+      'p[attribute ((())]. Right');
+
+  MT('divFormatting',
+      '[punctuation div. ][punctuation&strong *foo ]' +
+        '[punctuation&strong&em _bar_][punctuation&strong *]');
+
+  MT('phraseModifierAttributes',
+      'p[attribute (my-class)]. This is a paragraph that has a class and' +
+      ' this [em _][em&attribute (#special-phrase)][em emphasized phrase_]' +
+      ' has an id.');
+
+  MT('linkWithClass',
+      '[link "(my-class). This is a link with class":http://redcloth.org]');
+
+  /*
+   * Layouts
+   */
+
+  MT('paragraphLayouts',
+      'p. This is one paragraph.',
+      '',
+      'p. This is another.');
+
+  MT('div',
+      '[punctuation div. foo bar]');
+
+  MT('pre',
+      '[operator pre. Text]');
+
+  MT('bq.',
+      '[bracket bq. foo bar]',
+      '',
+      'Normal text.');
+
+  MT('footnote',
+      '[variable fn123. foo ][variable&strong *bar*]');
+
+  /*
+   * Spanning Layouts
+   */
+
+  MT('bq..ThenParagraph',
+      '[bracket bq.. foo bar]',
+      '',
+      '[bracket More quote.]',
+      'p. Normal Text');
+
+  MT('bq..ThenH1',
+      '[bracket bq.. foo bar]',
+      '',
+      '[bracket More quote.]',
+      '[header&header-1 h1. Header Text]');
+
+  MT('bc..ThenParagraph',
+      '[atom bc.. # Some ruby code]',
+      '[atom obj = {foo: :bar}]',
+      '[atom puts obj]',
+      '',
+      '[atom obj[[:love]] = "*love*"]',
+      '[atom puts obj.love.upcase]',
+      '',
+      'p. Normal text.');
+
+  MT('fn1..ThenParagraph',
+      '[variable fn1.. foo bar]',
+      '',
+      '[variable More.]',
+      'p. Normal Text');
+
+  MT('pre..ThenParagraph',
+      '[operator pre.. foo bar]',
+      '',
+      '[operator More.]',
+      'p. Normal Text');
+
+  /*
+   * Tables
+   */
+
+  MT('table',
+      '[variable-3&operator |_. name |_. age|]',
+      '[variable-3 |][variable-3&strong *Walter*][variable-3 |   5  |]',
+      '[variable-3 |Florence|   6  |]',
+      '',
+      'p. Normal text.');
+
+  MT('tableWithAttributes',
+      '[variable-3&operator |_. name |_. age|]',
+      '[variable-3 |][variable-3&attribute /2.][variable-3  Jim |]',
+      '[variable-3 |][variable-3&attribute \\2{color: red}.][variable-3  Sam |]');
+
+  /*
+   * HTML
+   */
+
+  MT('html',
+      '[comment <div id="wrapper">]',
+      '[comment <section id="introduction">]',
+      '',
+      '[header&header-1 h1. Welcome]',
+      '',
+      '[variable-2 * Item one]',
+      '[variable-2 * Item two]',
+      '',
+      '[comment <a href="http://example.com">Example</a>]',
+      '',
+      '[comment </section>]',
+      '[comment </div>]');
+
+  MT('inlineHtml',
+      'I can use HTML directly in my [comment <span class="youbetcha">Textile</span>].');
+
+  /*
+   * No-Textile
+   */
+
+  MT('notextile',
+    '[string-2 notextile. *No* formatting]');
+
+  MT('notextileInline',
+      'Use [string-2 ==*asterisks*==] for [strong *strong*] text.');
+
+  MT('notextileWithPre',
+      '[operator pre. *No* formatting]');
+
+  MT('notextileWithSpanningPre',
+      '[operator pre.. *No* formatting]',
+      '',
+      '[operator *No* formatting]');
+
+  /* Only toggling phrases between non-word chars. */
+
+  MT('phrase-in-word',
+     'foo_bar_baz');
+
+  MT('phrase-non-word',
+     '[negative -x-] aaa-bbb ccc-ddd [negative -eee-] fff [negative -ggg-]');
+
+  MT('phrase-lone-dash',
+     'foo - bar - baz');
 })();
