9d03a3fba7595ffd08eaa2ebf76953b3942f4165
Mlitliu0213 <130287136+Mlitliu0213@users.noreply.github.com>
2023-04-28 13:31:16 +0800

    Add files via upload

---
+++ StorageException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @copyright ©2020 于飞PHP在线客服系统
+* Created by yfi6.com
+ * User: Andy - Wangjie
+ * Date: 2020/6/17
+ * Time: 16:50
+ */
+
+namespace app\common\lib\storage;
+
+class StorageException extends \Exception
+{
+
+}
