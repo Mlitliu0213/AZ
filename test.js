fc05190cc1fd84426e2680b9c4850aa291e5d2ae
Mlitliu0213 <130287136+Mlitliu0213@users.noreply.github.com>
2023-04-28 16:27:41 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -1,97 +1,96 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
+// Slim Highlighting for CodeMirror copyright (c) HicknHack Software Gmbh
+
 (function() {
-  var mode = CodeMirror.getMode({tabSize: 4, indentUnit: 2}, "haml");
+  var mode = CodeMirror.getMode({tabSize: 4, indentUnit: 2}, "slim");
   function MT(name) { test.mode(name, mode, Array.prototype.slice.call(arguments, 1)); }
 
   // Requires at least one media query
   MT("elementName",
-     "[tag %h1] Hey There");
+     "[tag h1] Hey There");
 
   MT("oneElementPerLine",
-     "[tag %h1] Hey There %h2");
+     "[tag h1] Hey There .h2");
 
-  MT("idSelector",
-     "[tag %h1][attribute #test] Hey There");
+  MT("idShortcut",
+     "[attribute&def #test] Hey There");
 
-  MT("classSelector",
-     "[tag %h1][attribute .hello] Hey There");
+  MT("tagWithIdShortcuts",
+     "[tag h1][attribute&def #test] Hey There");
+
+  MT("classShortcut",
+     "[attribute&qualifier .hello] Hey There");
+
+  MT("tagWithIdAndClassShortcuts",
+     "[tag h1][attribute&def #test][attribute&qualifier .hello] Hey There");
 
   MT("docType",
-     "[tag !!! XML]");
+     "[keyword doctype] xml");
 
   MT("comment",
      "[comment / Hello WORLD]");
 
   MT("notComment",
-     "[tag %h1] This is not a / comment ");
+     "[tag h1] This is not a / comment ");
 
   MT("attributes",
-     "[tag %a]([variable title][operator =][string \"test\"]){[atom :title] [operator =>] [string \"test\"]}");
+     "[tag a]([attribute title]=[string \"test\"]) [attribute href]=[string \"link\"]}");
+
+  MT("multiLineAttributes",
+     "[tag a]([attribute title]=[string \"test\"]",
+     "  ) [attribute href]=[string \"link\"]}");
 
   MT("htmlCode",
      "[tag&bracket <][tag h1][tag&bracket >]Title[tag&bracket </][tag h1][tag&bracket >]");
 
   MT("rubyBlock",
-     "[operator =][variable-2 @item]");
+     "[operator&special =][variable-2 @item]");
 
   MT("selectorRubyBlock",
-     "[tag %a.selector=] [variable-2 @item]");
+     "[tag a][attribute&qualifier .test][operator&special =] [variable-2 @item]");
 
   MT("nestedRubyBlock",
-      "[tag %a]",
-      "   [operator =][variable puts] [string \"test\"]");
+      "[tag a]",
+      "  [operator&special =][variable puts] [string \"test\"]");
 
   MT("multilinePlaintext",
-      "[tag %p]",
-      "  Hello,",
-      "  World");
+      "[tag p]",
+      "  | Hello,",
+      "    World");
 
   MT("multilineRuby",
-      "[tag %p]",
-      "  [comment -# this is a comment]",
+      "[tag p]",
+      "  [comment /# this is a comment]",
       "     [comment and this is a comment too]",
-      "  Date/Time",
-      "  [operator -] [variable now] [operator =] [tag DateTime][operator .][property now]",
-      "  [tag %strong=] [variable now]",
-      "  [operator -] [keyword if] [variable now] [operator >] [tag DateTime][operator .][property parse]([string \"December 31, 2006\"])",
-      "     [operator =][string \"Happy\"]",
-      "     [operator =][string \"Belated\"]",
-      "     [operator =][string \"Birthday\"]");
+      "  | Date/Time",
+      "  [operator&special -] [variable now] [operator =] [tag DateTime][operator .][property now]",
+      "  [tag strong][operator&special =] [variable now]",
+      "  [operator&special -] [keyword if] [variable now] [operator >] [tag DateTime][operator .][property parse]([string \"December 31, 2006\"])",
+      "     [operator&special =][string \"Happy\"]",
+      "     [operator&special =][string \"Belated\"]",
+      "     [operator&special =][string \"Birthday\"]");
 
   MT("multilineComment",
       "[comment /]",
       "  [comment Multiline]",
       "  [comment Comment]");
 
-  MT("hamlComment",
-     "[comment -# this is a comment]");
-
-  MT("multilineHamlComment",
-     "[comment -# this is a comment]",
-     "   [comment and this is a comment too]");
-
-  MT("multilineHTMLComment",
-    "[comment <!--]",
-    "  [comment what a comment]",
-    "  [comment -->]");
-
   MT("hamlAfterRubyTag",
-    "[attribute .block]",
-    "  [tag %strong=] [variable now]",
-    "  [attribute .test]",
-    "     [operator =][variable now]",
-    "  [attribute .right]");
+    "[attribute&qualifier .block]",
+    "  [tag strong][operator&special =] [variable now]",
+    "  [attribute&qualifier .test]",
+    "     [operator&special =][variable now]",
+    "  [attribute&qualifier .right]");
 
   MT("stretchedRuby",
-     "[operator =] [variable puts] [string \"Hello\"],",
+     "[operator&special =] [variable puts] [string \"Hello\"],",
      "   [string \"World\"]");
 
   MT("interpolationInHashAttribute",
-     //"[tag %div]{[atom :id] [operator =>] [string \"#{][variable test][string }_#{][variable ting][string }\"]} test");
-     "[tag %div]{[atom :id] [operator =>] [string \"#{][variable test][string }_#{][variable ting][string }\"]} test");
+     "[tag div]{[attribute id] = [string \"]#{[variable test]}[string _]#{[variable ting]}[string \"]} test");
 
   MT("interpolationInHTMLAttribute",
-     "[tag %div]([variable title][operator =][string \"#{][variable test][string }_#{][variable ting]()[string }\"]) Test");
+     "[tag div]([attribute title]=[string \"]#{[variable test]}[string _]#{[variable ting]()}[string \"]) Test");
 })();
